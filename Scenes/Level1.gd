5dcfb85b939ba6a3c24ceab601b0a3c1d0c32f5d
Wowe-Peanut <47678647+Wowe-Peanut@users.noreply.github.com>
2021-12-08 14:51:11 -0600

    Starting to add dialogic

--- Scenes/Level1.gd
+++ Scenes/Level1.gd
@@ -1,1 +1,19 @@
 extends "res://Scenes/LevelBase.gd"
+
+
+func _ready():
+	if get_tree().get_root().get_node("Main").resets == 0:
+		var intro_1 = Dialogic.start("1_intro")
+		intro_1.connect("dialogic_signal", self, "dialog_listener")
+		intro_1.pause_mode = PAUSE_MODE_PROCESS
+		add_child(intro_1)
+		get_tree().paused = true
+	
+func _process(delta):
+	if get_tree().get_root().get_node("Main").resets == 10:
+		say("Dixie", "You have had to reset 10 times on the first level... wtf")
+	
+func dialog_listener(argument):
+	match argument:
+		"start_movement":
+			get_tree().paused = false
--- Scenes/LevelBase.gd
+++ Scenes/LevelBase.gd
@@ -1,5 +1,11 @@
 extends Node2D
 
-
 export var starting_water = 100
 export var water_required = 50
+var dialogue = Dialogic.start("say")
+
+func say(speaker, text):
+	Dialogic.set_variable("speaker", speaker)
+	Dialogic.set_variable("next_line", text)
+	add_child(dialogue)
+
